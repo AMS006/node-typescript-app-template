1d4e6b9cf5d0629290f47199e259a96754cc1ccd
Anas5006 <anassain13@gmail.com>
2023-10-29 10:53:42 +0530

    Add Express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-function print(name: string) {
-    return name;
-}
+import app from './app';
+import { Config } from './config';
 
-print("Anas");
+const startServer = () => {
+    const PORT = Config.PORT;
+
+    try {
+        app.listen(PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(`Listening on ${PORT}`);
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            // eslint-disable-next-line no-console
+            console.log(error.message);
+            process.exit(1);
+        }
+    }
+};
+
+startServer();
